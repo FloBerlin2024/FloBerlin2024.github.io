31ee9a5d0956a7b2ce3849860f4427ca87370ad1
Florian Matthias <florian@MacBookAir.fritz.box>
2026-07-19 09:47:01 +0200

    PWA v61: grüner Hintergrund erreichter Abschluss-Boxen wiederhergestellt

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *
  * Cache-Version bei jeder Auslieferung erhöhen -> alter Cache wird ersetzt.
  */
-const CACHE = 'tgs-abschlussrechner-v60';
+const CACHE = 'tgs-abschlussrechner-v61';
 
 // Relativ zu /…/PWA/sw.js. '../vendor' und 'favicon.png' liegen im Projekt-Root.
 const PRECACHE = [
